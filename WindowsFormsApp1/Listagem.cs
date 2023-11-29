daae41c26d715dd58219d34721f4a3c9d3936717
fnascimento <felipegonsalves168@gmail.com>
2023-11-29 15:15:56 -0300

    Adicionar arquivos de projeto.

---
+++ WindowsFormsApp1/Listagem.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace WindowsFormsApp1
+{
+    public partial class Listagem : Form
+    {
+        public static List<string> userValues { get; set; }
+        public string usuarios()
+        {            
+            return userValues[0];
+        }
+
+        public string ValorDoTextBox
+        {
+            get { return textBox1.Text; }
+        }
+        public Listagem()
+        {
+            InitializeComponent();
+        }
+      
+        private void listBox1_SelectedIndexChanged(object sender, EventArgs e)
+        {
+            
+            
+        }
+
+        private void button_back(object sender, EventArgs e)
+        {
+            this.Hide();
+            new Menu().Show();
+        }
+    }
+}
---
+++ WindowsFormsApp1/Menu.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace WindowsFormsApp1
+{
+    public partial class Menu : Form
+    {
+        public Menu()
+        {
+            InitializeComponent();
+        }
+
+        private void button_registros(Object sander, EventArgs e)
+        {
+            new Listagem().Show();
+            this.Hide();
+        }
+        private void button_criar(Object sander, EventArgs e)
+        {
+            new RegistrosForm().Show();
+            this.Hide();
+        }
+        private void button_editar(Object sander, EventArgs e)
+        {
+
+        }
+    }
+}
